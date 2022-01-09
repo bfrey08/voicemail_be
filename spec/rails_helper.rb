128ce0918356c63f08a572d5ccdd345f156ddba5
Nathan Brown <nathan.brown263@gmail.com>
2022-01-08 23:11:02 -0700

    fix: Remove irrelevant folders from SimpleCov coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter 'app/channels'
+  add_filter 'app/jobs'
+  add_filter 'app/mailers'
+end
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'

1c0597a01aad9a6deaab3e49f17dae565ad05212
Nathan Brown <nathan.brown263@gmail.com>
2022-01-09 15:23:36 -0700

    feat: Add titles of representatives to name attribute

--- app/facades/google_facade.rb
+++ app/facades/google_facade.rb
@@ -1,10 +1,19 @@
 class GoogleFacade
   def self.representatives(address)
-    representatives = GoogleService.find_representatives(address)[:officials]
-    representatives.map.with_index do |rep, index|
+    representatives = GoogleService.find_representatives(address)
+    reps = representatives[:officials].map.with_index do |rep, index|
       representative = Representative.new(rep)
       representative.set_id(index)
       representative
     end
+
+    representatives[:offices].each do |office|
+      reps.each_with_index do |rep, index|
+        if office[:officialIndices].include?(index)
+          rep.add_title(office[:name])
+        end
+      end
+    end
+    reps
   end
 end

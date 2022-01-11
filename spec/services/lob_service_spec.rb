0205ca544c1e14dd96d98c8411843a12285cbda9
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-10 17:38:03 -0700

    Test - Add verification client to lob service tests

--- spec/services/lob_service_spec.rb
+++ spec/services/lob_service_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 describe LobService do
   it 'can make a new lob object' do
     lob = LobService.client
+    lob2 = LobService.verification_client
 
     expect(lob).to be_an_instance_of(Lob::Client)
+    expect(lob2).to be_an_instance_of(Lob::Client)
   end
 
 

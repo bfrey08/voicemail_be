df4f34e63c0b525794df50a6e76454b7b0fe17ec
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-10 17:37:38 -0700

    Test - Add test to verify addresses in lob facade

--- spec/facades/lob_facade_spec.rb
+++ spec/facades/lob_facade_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 describe LobFacade do
+  describe '.verify_address' do
+    it 'returns true if an address can be delivered to' do
+      address = {
+        name: "Burger King",
+        address_line1: "2110 S Broadway",
+        address_city: "Denver",
+        address_state: "CO",
+        address_country: "US",
+        address_zip: "80210"
+      }
+
+      expect(LobFacade.verify_address(address)).to eq(true)
+    end
+
+    it 'returns false if an address cant be delivered to' do
+      address = {
+        name: "alspdgfpcalsd",
+        address_line1: "vasadcasdgasdg",
+        address_city: "sadmvodmc",
+        address_state: "pwefoqckds",
+        address_country: "kaosdvkospck",
+        address_zip: "43951"
+      }
+
+      expect(LobFacade.verify_address(address)).to eq(false)
+    end
+  end
+
   describe '.create_letter' do
     context 'when required information entered' do
       it 'sends letter and returns saved letter object' do
@@ -66,7 +94,7 @@ describe LobFacade do
                      }
 
         letter = LobFacade.create_letter({to_address: to_address, from_address: from_address, user: nate})
-        
+
         expect(letter.id).to be nil
         expect(letter.errors.messages[:body]).to eq(["can't be blank"])
       end
